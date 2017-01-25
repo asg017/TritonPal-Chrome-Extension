7325a2af81268b768cdd2c8141dfd1549836818f
Alex Garcia <alexsebastian.garcia@gmail.com>
2017-01-24 23:48:07 -0800

    button better changes

--- apps/calendar/inject.js
+++ apps/calendar/inject.js
@@ -1,47 +1,49 @@
-console.log('ayyy');
-
-
-var topDiv = document.createElement('input');
-
-topDiv.style.position = 'inline';
-topDiv.type = 'button';
-topDiv.value = 'Convert to gCal';
-topDiv.style.marginLeft = '1px';
-topDiv.onclick = function() {alert('ayy lmao');};
-
-//topDiv.style.top = '200px';
-//topDiv.style.left = '20px';
-topDiv.style.height = '20px';
-topDiv.style.width = '120px';
-topDiv.style.backgroundColor = 'yellow';
-topDiv.style.zIndex = 100;
-
-document.getElementById('schedule-addevent-div').appendChild(topDiv);
-
-
 
 //1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
+var gCalButton = document.createElement('button');
+gCalButton.innerText = 'Export';
 
-// 2. Append somewhere
-/*
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button); */
-document.getElementById('schedule-addevent-div').appendChild(button);
+var gCalStyles = {
+    width: '90px',
+    height: '20px',
+    'font-size': '12px',
+    'font-weight': 'bold',
+    'margin-left': '10px',
+    border: 0,
+    color: '#333',
+    display: 'inline-block',
+    outline: 0,
+    cursor: 'pointer',
+    'text-align': 'center',
+    'text-decoration': 'none',
+    'text-shadow': 'rgba(255,255,255,.5) 0 1px 1px',
+    'margin-right': '.5em',
+    'padding': '.25em 1em',
+    '-moz-border-radius': '.25em',
+    '-webkit-border-radius': '.25em',
+    'border-radius': '.25em',
+    '-moz-box-shadow': '0 1px 2px rgba(0,0,0,.5)',
+    '-webkit-box-shadow': '0 1px 2px rgba(0,0,0,.5)',
+    'boxshadow': '0 1px 2px rgba(0,0,0,.5)'
+};
+
+for(var x in gCalStyles) {
+	gCalButton.style[x] = gCalStyles[x];
+}
+
+gCalButton.className = 'button secondary';
+
+document.getElementById('schedule-addevent-div').appendChild(gCalButton);
 
 // 3. Add event handler
-button.addEventListener ("click", function() {
+gCalButton.addEventListener ("click", function() {
   //Save BookList as a variable
   var booklink = document.getElementById("viewbooklistlink").getAttribute("href");
   var length = "https://ucsdbkst.ucsd.edu/wrtx/FullBookList".length; //44
 
-  //Alert the BookList link
-  alert(booklink);
 
   //Get valuable information from booklink
   var infolink = booklink.substring(length);
-
   //Open in a new tab
   window.open('https://tritonpal.herokuapp.com/apps/calendar/landing.html' +
               infolink, '_blank');
